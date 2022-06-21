f60cc5c8ac65f02791e44e246c1f1d38adc7a034
Michael Thomas <geofire155@gmail.com>
2022-06-21 09:39:41 -0400

    Woo! Mostly meaningless changes

--- SMRequest-Client/main.js
+++ SMRequest-Client/main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -6,23 +7,26 @@ function createWindow () {
     height: 600,
     icon: __dirname + '/images/icon.ico',
     webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 }
 
-app.whenReady().then(createWindow)
+// when app has finished preparing, create the window
+app.whenReady().then(() => {
+  createWindow();
 
+  // on macOS, create the window on activation
+  app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// stop process when all windows are closed
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+  // close application unless macOS
+  if (process.platform !== 'darwin') app.quit();
+});
